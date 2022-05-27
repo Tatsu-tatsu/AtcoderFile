54b4f3a53459f4e64df0136f96548304a37db0d8
Tatsu-tatsu <tatsu.nakagawa7@gmail.com>
2022-05-27 16:00:40 +0900

    2022/05/27

--- AIZUJUDGE/Introduction/7_D.py
+++ AIZUJUDGE/Introduction/7_D.py
@@ -1,3 +1,6 @@
+# 行列の問題はpythonのnumpyというライブラリを使うと楽に計算できる。
+# np.array()で多次元配列で計算ができる。
+
 import numpy as np
 n, m, l = map(int, input().split())
 
@@ -10,8 +13,23 @@ for i in range(n):
 for j in range(m):
   B.append(list(map(int, input().split())))
 
-print(A)
-print(B)
+C = np.dot(A, B)
+for i in range(n):
+  for j in range(l):
+    print(C[i][j], end="")
+  print("")
 
+# 下arrayとmatrixを使った計算方法(matrixは行列に特化したものだが扱いにくいかも)
+A2 = np.array(A)
+B2 = np.array(B)
+# or
 A2 = np.matrix(A)
-print(A2)
+B2 = np.matrix(B)
+# ここは行列の掛け算
+C = np.dot(A2, B2)
+# matrixから配列にもどす。
+C2 = np.array(C)
+for i in range(n):
+  for j in range(l):
+    print(C2[i][j], end="")
+  print("")
