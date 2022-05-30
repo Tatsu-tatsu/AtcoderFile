87d22632cb92e9c049ad6424e735fcac9c29654b
Tatsu-tatsu <tatsu.nakagawa7@gmail.com>
2022-05-30 19:31:51 +0900

    2022/05/30

--- AIZUJUDGE/DPL/1_B.py
+++ AIZUJUDGE/DPL/1_B.py
@@ -14,7 +14,17 @@ dp = [[INF for i in range(W+1)] for j in range(N+1)]
 for i in range(W+1):
   dp[0][i] = 0
 
+for i in range(N+1):
+  dp[i][0] = 0
 
+for i in range(1, N+1):
+  for j in range(1, W+1):
+    if j - items[i-1][1] >= 0:
+      dp[i][j] = max(dp[i-1][j], dp[i-1][j - items[i-1][1]] + items[i-1][0])
+    else:
+      dp[i][j] = dp[i-1][j]
+
+print(dp[N][W])
 
 
 
